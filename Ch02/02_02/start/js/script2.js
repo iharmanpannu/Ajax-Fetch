e31daa3144a1b9902e11230d6c2286eab8ceb829
Harman Pannu <iharmanpannu@gmail.com>
2018-03-02 14:07:26 -0800

    writing code with fetch now

--- Ch02/02_02/start/js/script2.js
+++ Ch02/02_02/start/js/script2.js
@@ -2,45 +2,11 @@
   const url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
   const apiKey = "05e62f86576a85bc20cce0e46674216f"; // Replace "API KEY" with your own API key; otherwise, your HTTP request will not work
   let httpRequest;
-  makeRequest();
 
-  // Create and send XHR Request
-
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = responseMethod;
-    httpRequest.open("GET", `${url}&appid=${apiKey}`);
-    httpRequest.send();
-  }
-
-  // Handle XHR response
-
-  function responseMethod() {
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        updateUISuccess(httpRequest.responseText);
-      } else {
-        updateUIError();
-      }
+  fetch(`${url}&appid=${apiKey}`).then(function(response) {
+    if (!response.ok) {
+      console.log("Problem");
     }
-  }
-
-  // Handle XHR success
-  function updateUISuccess(responseText) {
-    const response = JSON.parse(responseText);
-    const condition = response.weather[0].main;
-    const degC = response.main.temp - 273.15;
-    const degCInt = Math.log.floor(degC);
-    const degF = degC * 1.8 + 32;
-    const degFInt = Math.floor(degF);
-    const weatherBox = document.getElementById("weather");
-    weatherBox.innerHTML = `<p>${degCInt}&#176; C / ${degFInt}&#176; F</p> <p>${condition}</p>`;
-  }
-
-  // handle XHR error
-
-  function updateUIError() {
-    const weatherBox = document.getElementById("weather");
-    weatherBox.className = "hidden";
-  }
+    console.log(response);
+  });
 })();
