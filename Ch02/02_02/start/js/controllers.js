4c28ef84c6c49c555d779ed150a3b3a3250ad601
Harman Pannu <iharmanpannu@gmail.com>
2018-03-03 10:55:24 -0800

    changes

--- Ch02/02_02/start/js/controllers.js
+++ Ch02/02_02/start/js/controllers.js
@@ -1,7 +1,14 @@
-angular.module('listApp', [])
-    .controller('ListController', ['$http', function ($http) {
-        $http.get('https://www.eventbriteapi.com/v3/events/search/?token=K3WIYUKJBMKJAKZADRGG&location.address=seattle+usa')
-            .then(function (data) {
-                console.log(data)
-            })
-    }])
+angular.module("listApp", []).controller("ListController", [
+  "$http",
+  function($http) {
+    const vm = this;
+    let events = [];
+    $http
+      .get(
+        "https://www.eventbriteapi.com/v3/events/search/?token=K3WIYUKJBMKJAKZADRGG&location.address=london+uk"
+      )
+      .success(function(data) {
+        vm.events = data.events;
+      });
+  }
+]);
