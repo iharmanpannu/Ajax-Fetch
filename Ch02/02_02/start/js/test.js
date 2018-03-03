8d1acd5752065273d193055f3f306192b55ad058
Harman Pannu <iharmanpannu@gmail.com>
2018-03-02 23:53:29 -0800

    adding jquery to handle fetch request

--- Ch02/02_02/start/js/test.js
+++ Ch02/02_02/start/js/test.js
@@ -71,3 +71,37 @@ function updateUIError() {
 //   .catch(function() {
 //     // do something with error
 //   });
+
+// fetch AJAX request
+
+fetch(`${url}&appid=${apiKey}`)
+  .then(function(response) {
+    if (!response.ok) {
+      throw Error(response.text);
+    }
+    return response.json();
+  })
+  .then(function(response) {
+    updateUISuccess(response);
+  })
+  .catch(function(error) {
+    updateUIError();
+  });
+
+// Handle XHR success
+function updateUISuccess(response) {
+  const condition = response.weather[0].main;
+  const degC = response.main.temp - 273.15;
+  const degCInt = Math.floor(degC);
+  const degF = degC * 1.8 + 32;
+  const degFInt = Math.floor(degF);
+  const weatherBox = document.getElementById("weather");
+  weatherBox.innerHTML = `<p>${degCInt}&#176; C / ${degFInt}&#176; F</p> <p>${condition}</p>`;
+}
+
+// handle XHR error
+
+function updateUIError() {
+  const weatherBox = document.getElementById("weather");
+  weatherBox.className = "hidden";
+}
